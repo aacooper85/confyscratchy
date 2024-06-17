f8c8c90705aa547991c9c7f9d7f1ba89a4677dbc
ejherzog <23402385+ejherzog@users.noreply.github.com>
2024-06-16 23:08:35 -0400

    puzzle favicon, title

--- src/routes/credits/+page.svelte
+++ src/routes/credits/+page.svelte
@@ -3,5 +3,6 @@
 <ul>
     <li><p><a href="https://github.com/aacooper85/confeescratchee" target="_blank">ConfeeScratchee by aacooper85</a></p></li>
     <li><p><a href="https://github.com/StevenClontz/scratchee" target="_blank">Scratchee by StevenClontz</a></p></li>
+    <li><p>Favicon: <a href="https://www.flaticon.com/free-icon/puzzle_3625145" target="_blank">Puzzle Icon</a> by <a href="https://www.flaticon.com/authors/icon-home" target="_blank">Icon Home</a></p></li>
     <li><p>Favicon: <a href="https://thenounproject.com/icon/question-6828513/" target="_blank">Question Icon</a> by <a href="https://thenounproject.com/creator/lewen.design" target="_blank">Lewen Design</a> from Noun Project</p></li>
 </ul>
