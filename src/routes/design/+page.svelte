880f3191b0100a1af9d9f56080b7f5df09dbfd94
ejherzog <23402385+ejherzog@users.noreply.github.com>
2024-06-16 22:56:57 -0400

    qr code

--- src/routes/design/+page.svelte
+++ src/routes/design/+page.svelte
@@ -1,11 +1,32 @@
 <script>
 	/** @type {import('./$types').PageData} */
 	export let data;
+	
+	let cardId = data.cardId;
+	let encodedData = btoa(cardId);
+	let confUrl = `/confidence/${encodedData}`;
+
+	function newCardId() {
+		cardId = crypto.randomUUID();
+		encodedData = btoa(cardId);
+		confUrl = `/confidence/${encodedData}`;
+	}
+
+	import { QRCodeImage } from "svelte-qrcode-image";
 </script>
 
 <h1>Design A New Card</h1>
 
-<h4><a href="/confidence/{data.confUrl}">Confidence URL</a></h4>
-<p>Card ID: {data.cardId}</p>
+<h4><a href={confUrl}>Confidence URL</a></h4>
+
+<QRCodeImage text={confUrl} />
+
+<br>
+<button on:click={newCardId}>
+	Generate New Id
+</button>
+
+<p>Card ID: {cardId.substring(0, 6)}</p>
+<p>Encoded ID: {encodedData}</p>
 
 <p>~Card Design Components Go Here~</p>
--- src/routes/design/+page.ts
+++ src/routes/design/+page.ts
@@ -1,7 +1,6 @@
 /** @type {import('./$types').PageLoad} */
 export function load() {
     return {
-        cardId: crypto.randomUUID(),
-        confUrl: "098234jnr0dsf8oqwoe9rfjasfadsf"
+        cardId: crypto.randomUUID()
     };
 }
