924d29b09f63788ad1d920d035c3f8fcf520fd94
ejherzog <23402385+ejherzog@users.noreply.github.com>
2024-06-26 11:55:45 -0400

    add/remove options; reset answer if answer is removed

--- src/lib/components/design/Row.svelte
+++ src/lib/components/design/Row.svelte
@@ -7,11 +7,25 @@
     let answerChoice = (index: number) => () => {
         row.answer = index;
     }
+
+    let addOption = () => () => {
+        row.length += 1;
+    }
+
+    let removeOption = () => () => {
+        row.length -= 1;
+        if (row.answer >= row.length) {
+            row.answer = 0;
+        }
+    }
 </script>
 
-<p><b>{label}</b>
-{#each Array(row.length) as _,i}
-    <ConfidenceButton index={i} answer={row.answer} on:click={answerChoice(i)}></ConfidenceButton>
-{/each}
+<p>
+    <button on:click={addOption()} disabled={row.length>=26}>⬆️</button>
+    <button on:click={removeOption()} disabled={row.length<=1}>⬇️</button>
+    <b>{label}</b>
+    {#each Array(row.length) as _,i}
+        <ConfidenceButton index={i} answer={row.answer} on:click={answerChoice(i)}></ConfidenceButton>
+    {/each}
 <br>
 </p>
