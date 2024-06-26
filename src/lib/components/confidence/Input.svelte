c1c03713cffc2d9d96dddf08483c9c840977db2c
Eva J Herzog <ejherzog@users.noreply.github.com>
2024-06-26 12:00:05 -0400

    Dynamic Row Length (#2)
    * add/remove options; reset answer if answer is removed
    
    * label answers on confidence page
    
    ---------
    
    Co-authored-by: ejherzog <23402385+ejherzog@users.noreply.github.com>

--- src/lib/components/confidence/Input.svelte
+++ src/lib/components/confidence/Input.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
+    export let index: number;
     export let total: number;
     export let value: number;
 </script>
 
+<b>{String.fromCharCode(97 + index).toUpperCase()}:</b>
 <input type="number" min="0" max={total} bind:value={value}/>
