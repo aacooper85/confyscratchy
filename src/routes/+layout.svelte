178f6a67357d6f517a80eacf888aa4bf2a51b916
aacooper85 <67913751+aacooper85@users.noreply.github.com>
2024-07-25 11:41:51 -0400

    update layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,6 +8,6 @@
 <slot></slot>
 
 <hr>
-<p>A rebuild of ConfeeScratchee by <a href="https://github.com/ejherzog" target="_blank">ejherzog</a>.</p>
+<p>A rebuild of ConfeeScratchee by <a href="https://github.com/ejherzog" target="_blank">ejherzog</a> and <a href="https://github.com/aacooper85">aacooper85</a>.</p>
 <p><a href="/credits">Credits</a></p>
 
